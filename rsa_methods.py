2246333125a342c42e8133655ea374e247d7ded8
Balázs Tóth <gawnoris@gmail.com>
2021-05-06 15:38:43 +0200

    Update rsa_methods.py

--- rsa_methods.py
+++ rsa_methods.py
@@ -16,9 +16,23 @@ def RSA_run_enc(m,d,e,n,p,q):
 
 def RSA_run_dec(c,d,n,p,q):
     message = dec(c,d,n)
-    mp =dec(c,d,p)
-    mq =dec(c,d,q)
-    return mp
+    return message
+
+
+def RSA_run_decCRT(c,d,p,q):
+    c1= c % p
+    c2= c % q
+
+    dp= d % (p-1)
+    dq= d % (q-1)
+
+    mp= dec(c1,dp,p)
+    mq =dec(c2,dq,q)
+
+    qinv = modular_functions.modinvers(q,p)
+    h = (qinv * (mp - mq)) % p
+    m = mq + h * q
+    return m
 
 
 def RSA_for_message_enc(intlist,d,e,n,p,q):
@@ -34,4 +48,12 @@ def RSA_for_message_dec(encrypted_int_list,d,n,p,q):
     for integer in encrypted_int_list:
         mp = RSA_run_dec(integer,d,n,p,q)
         decrypted_int_list.append(mp)
+    return decrypted_int_list
+
+
+def RSA_for_message_decCRT(encrypted_int_list,d,p,q):
+    decrypted_int_list = []
+    for integer in encrypted_int_list:
+        m = RSA_run_decCRT(integer,d,p,q)
+        decrypted_int_list.append(m)
     return decrypted_int_list
